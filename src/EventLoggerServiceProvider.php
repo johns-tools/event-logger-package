950bd62ffd46d2c6b59d03977dbe2c88b44125db
johns-tools <johns.development.projects@gmail.com>
2024-03-09 12:31:26 +0000

    tweak to the routes filename and location.

--- src/EventLoggerServiceProvider.php
+++ src/EventLoggerServiceProvider.php
@@ -13,7 +13,7 @@ class EventLoggerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
     }
 
     /**
